cfbfa10469876977efde923d46a1902774320b98
Gautzilla <gauthier.berthomieu@gmail.com>
2024-07-26 10:06:29 +0200

    add multi axes plot

--- src/utils_manip_directivite/analysis/visualization.py
+++ src/utils_manip_directivite/analysis/visualization.py
@@ -3,19 +3,35 @@ import seaborn as sns
 import pandas as pd
 from utils_manip_directivite.analysis.extract_data import get_dataframe
 
-def plot(df: pd.DataFrame):
+def plot_results(df: pd.DataFrame, question: str):
     sns.set_theme()
-    sns.catplot(x = 'movement',
-                y = 'answer_timbre',
-                col = 'angle',
+    f, axs = plt.subplots(nrows = 2, ncols = 2, sharey = True)
+    sns.boxplot(x = 'movement',
+                y = question,
                 hue = 'source',
-                data = df)
+                data = df.query('room == "SUAPS" and angle == "Front"'),
+                ax = axs[0,0])
+    sns.boxplot(x = 'movement',
+                y = question,
+                hue = 'source',
+                data = df.query('room == "CLOUS" and angle == "Front"'),
+                ax = axs[0,1])
+    sns.boxplot(x = 'movement',
+                y = question,
+                hue = 'source',
+                data = df.query('room == "SUAPS" and angle == "Side"'),
+                ax = axs[1,0])
+    sns.boxplot(x = 'movement',
+                y = question,
+                hue = 'source',
+                data = df.query('room == "CLOUS" and angle == "Side"'),
+                ax = axs[1,1])
     plt.show()
 
 def main() -> None:
     df = get_dataframe()
-    df = df.query('user > 1')
-    plot(df)
+    df = df.query('user > 1').sort_values(by=['source'])
+    plot_results(df = df, question = 'answer_timbre')
 
 if __name__ == '__main__':
     main()
