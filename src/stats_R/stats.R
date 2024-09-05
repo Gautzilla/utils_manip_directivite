dddfa2bb5ced24e0f56c9bce377ba40742037e87
Gautzilla <gauthier.berthomieu@gmail.com>
2024-09-05 11:58:02 +0200

    new participants

--- src/stats_R/stats.R
+++ src/stats_R/stats.R
@@ -37,7 +37,7 @@ df <- df %>%
 # Merge Movement and Amplitude factors
 df$stimulus <- paste(df$movement, df$amplitude)
 df <- df %>%
-  mutate(stimulus = recode(stimulus, '0 Small' = 'Static_1', '0 Large' = 'Static_2', '1 Small' = 'Dynamic_1', '1 Large' = 'Dynamic_2'))
+  mutate(stimulus = recode(stimulus, '0 Small' = 'Sta1', '0 Large' = 'Sta2', '1 Small' = 'Dyn1', '1 Large' = 'Dyn2'))
 
 df <- df[, c('id', 'room', 'distance', 'angle', 'stimulus', 'source', 'answer_plausibility', 'answer_timbre', 'answer_angle', 'answer_movement')]
 
@@ -56,10 +56,19 @@ defaultTheme <- theme_set(theme_minimal()) +
 theme_set(defaultTheme)
 
 # Correlation
-corr <- df %>%
-  dplyr::summarize(correlation = cor(answer_plausibility, answer_timbre))
 
+plot(df$answer_plausibility, df$answer_timbre)
+
+df %>%
+  dplyr::summarize(correlation = cor(answer_plausibility, answer_timbre))
+cor.test(df$answer_plausibility, df$answer_timbre)
 # Boxplots
+df %>%
+  ggplot(aes(source, answer_plausibility, colour = stimulus)) +
+  facet_grid(room ~ angle + distance) +
+  geom_boxplot(width = .1) +
+  coord_cartesian(ylim=c(0.,1.))
+
 fig_boxplot_plausibility <- df %>%
   ggplot(aes(movement, answer_plausibility, colour = source)) +
   facet_grid(room ~ angle) +
@@ -110,13 +119,13 @@ outliers_timbre <- df %>%
 # Check Normality per cell (residuals are checked after the ANOVA)
 
 shapiroWilk_plausibility <- df %>%
-  group_by(room, source, angle, movement, distance, amplitude) %>%
+  group_by(room, source, angle, stimulus, distance) %>%
   shapiro_test(answer_plausibility) %>%
   mutate(Sigp = ifelse(p > 0.05, "" , ifelse(p > 0.01, "*", ifelse(p > 0.01, "**", "***")))) %>%
   round_num(3)
 
 shapiroWilk_timbre <- df %>%
-  group_by(room, source, angle, movement, distance, amplitude) %>%
+  group_by(room, source, angle, stimulus, distance) %>%
   shapiro_test(answer_timbre) %>%
   mutate(Sigp = ifelse(p > 0.05, "" , ifelse(p > 0.01, "*", ifelse(p > 0.01, "**", "***")))) %>%
   round_num(3)
@@ -126,20 +135,22 @@ shapiroWilk_timbre <- df %>%
 ## PLAUSIBILITY
 
 anovaResults_plausibility <- df %>%
-  anova_test(dv = answer_plausibility, wid = id, within = c(room, source, angle, movement, distance)) 
+  anova_test(dv = answer_plausibility, wid = id, within = c(room, source, angle, stimulus, distance)) 
 
 anovaResultsCorrected_plausibility <- get_anova_table(anovaResults_plausibility, correction = "auto") %>%
   round_num(3)
 
-# xtable(anovaResultsCorrected_plausibility, digits = c(0,0,1,1,1,3,3,3))
+xtable(anovaResultsCorrected_plausibility, digits = c(0,0,1,1,1,3,3,3))
 
 ## TIMBRE
 
 anovaResults_timbre <- df %>%
-  anova_test(dv = answer_timbre, wid = id, within = c(room, source, angle, movement, distance)) 
+  anova_test(dv = answer_timbre, wid = id, within = c(room, source, angle, stimulus, distance)) 
 
 anovaResultsCorrected_timbre <- get_anova_table(anovaResults_timbre, correction = "auto") %>%
   round_num(3)
+
+xtable(anovaResultsCorrected_timbre, digits = c(0,0,1,1,1,3,3,3))
 
 # ANOVA Residuals
 
@@ -169,164 +180,79 @@ fig_qqPlotResiduals_timbre <- groupedResiduals_timbre %>%
 
 # Simple effects of the source figure
 
-fig_source_timbre <- df %>%
+df %>%
   ggplot(aes(source, answer_timbre)) +
   stat_summary(fun = mean, geom = "point", position = position_dodge(width = .1)) +
   stat_summary(fun.data = mean_cl_normal, geom = "errorbar", linewidth = 1, width = .1, position = "dodge") +
   coord_cartesian(ylim=c(-3.,3.))
 
-fig_source_plausibility <- df %>%
+df %>%
   ggplot(aes(source, answer_plausibility)) +
   stat_summary(fun = mean, geom = "point", position = position_dodge(width = .1)) +
   stat_summary(fun.data = mean_cl_normal, geom = "errorbar", linewidth = 1, width = .1, position = "dodge") +
   coord_cartesian(ylim=c(-3.,3.))
 
-# Plausibility interaction: Angle x Amplitude
+# Plausibility interaction: Source x Stimulus
 df %>%
-  group_by(angle) %>%
-  pairwise_t_test(answer_plausibility ~ movement, paired = TRUE) %>%
-  xtable(digits = c(0, 0, 0, 0, 0, 0, 0, 3, 0, 3, 3, 0))
-
-df %>%
-  group_by(angle) %>%
-  cohens_d(answer_plausibility ~ movement, paired = TRUE)
-
-# xtable(pl_tri, digits = c(0,0,0,0,0,0,0,0,2,0,3,3,0))
-
-
-df %>%
-  ggplot(aes(angle, answer_plausibility, colour = amplitude)) +
+  ggplot(aes(source, answer_plausibility, colour = stimulus)) +
   stat_summary(fun = mean, geom = "point", position = position_dodge(width = .1)) +
   stat_summary(fun.data = mean_cl_normal, geom = "errorbar", linewidth = 1, width = .1, position = "dodge") +
   coord_cartesian(ylim=c(-1.,1.))
-
-# Plausibility interaction: Room x Source x Movement post-hoc (movement moderator)
-pl_tri <- df %>%
-  group_by(source, room) %>%
-  pairwise_t_test(answer_plausibility ~ movement, paired = TRUE, p.adjust.method = "bonferroni")
-
-# xtable(pl_tri, digits = c(0,0,0,0,0,0,0,0,2,0,3,3,0))
-
-df %>%
-  ggplot(aes(source, answer_plausibility, colour = movement)) +
-  stat_summary(fun = mean, geom = "point", position = position_dodge(width = .1)) +
-  stat_summary(fun.data = mean_cl_normal, geom = "errorbar", linewidth = 1, width = .1, position = "dodge") +
-  coord_cartesian(ylim=c(-1.,1.))
-
-df %>%
-  ggplot(aes(room, answer_plausibility, colour = distance)) +
-  stat_summary(fun = mean, geom = "point", position = position_dodge(width = .1)) +
-  stat_summary(fun.data = mean_cl_normal, geom = "errorbar", linewidth = 1, width = .1, position = "dodge") +
-  coord_cartesian(ylim=c(-1.,1.))
-
-df %>%
-  ggplot(aes(movement, answer_plausibility, colour = distance)) +
-  facet_wrap(. ~ angle) +
-  stat_summary(fun = mean, geom = "point", position = position_dodge(width = .1)) +
-  stat_summary(fun.data = mean_cl_normal, geom = "errorbar", linewidth = 1, width = .1, position = "dodge") +
-  coord_cartesian(ylim=c(0.,1.))
-
-# Plausibility interaction: Angle x Movement x Distance post-hoc (movement moderator)
-df %>%
-  group_by(distance, movement) %>%
-  pairwise_t_test(answer_plausibility ~ angle, paired = TRUE, p.adjust.method = "bonferroni")
 
 ph <- df %>%
   group_by(source) %>%
-  pairwise_t_test(answer_plausibility ~ movement, paired = TRUE, p.adjust.method = "bonferroni")
+  pairwise_t_test(answer_plausibility ~ stimulus, paired = TRUE)
+ph$p_adj <- ph$p * 12
 
-xtable(pl_ph, digits = c(0,0,0,0,0,0,0,0,2,0,3,3,0))
+cohens <- df %>%
+  group_by(source) %>%
+  cohens_d(answer_plausibility ~ stimulus, paired = TRUE)
+
+ph_tot <- cbind(x = ph[, c('source', 'group1', 'group2', 'p_adj')], y = cohens[, c('effsize')])
+
+xtable(ph_tot, digits = c(0, 0, 0, 0, 3, 3))
+
+# Plausibility interaction: Angle x Stimulus x Distance
 df %>%
-  group_by(distance, angle) %>%
-  cohens_d(answer_plausibility ~ movement, paired = TRUE)
-
-df %>%
-  ggplot(aes(source, answer_plausibility, colour = movement)) +
-  facet_wrap(. ~ room) +
-  stat_summary(fun = mean, geom = "point", position = position_dodge(width = .1)) +
-  stat_summary(fun.data = mean_cl_normal, geom = "errorbar", linewidth = 1, width = .1, position = "dodge") +
-  coord_cartesian(ylim=c(-1.,1.))
-
-df %>%
-  ggplot(aes(angle, answer_timbre, colour = room)) +
-  facet_wrap(. ~ source) +
-  stat_summary(fun = mean, geom = "point", position = position_dodge(width = .1)) +
-  stat_summary(fun.data = mean_cl_normal, geom = "errorbar", linewidth = 1, width = .1, position = "dodge") +
-  coord_cartesian(ylim=c(-1.,1.))
-
-df %>%
-  ggplot(aes(movement, answer_movement, colour = source)) +
-  facet_wrap(amplitude ~ angle) +
-  stat_summary(fun = mean, geom = "point", position = position_dodge(width = .1)) +
-  stat_summary(fun.data = mean_cl_normal, geom = "errorbar", linewidth = 1, width = .1, position = "dodge") +
-  coord_cartesian(ylim=c(0.,1.))
-
-# Timbre interaction: RxSxTh
-
-ph <- df %>%
-  group_by(source, room) %>%
-  pairwise_t_test(answer_timbre ~ angle, paired = TRUE, p.adjust.method = "bonferroni")
-xtable(ph, digits = c(0,0,0,0,0,0,0,0,2,0,3,3,0))
-
-df %>%
-  ggplot(aes(room, answer_timbre, colour = angle)) +
-  facet_wrap(. ~ source) +
-  stat_summary(fun = mean, geom = "point", position = position_dodge(width = .1)) +
-  stat_summary(fun.data = mean_cl_normal, geom = "errorbar", linewidth = 1, width = .1, position = "dodge") +
-  coord_cartesian(ylim=c(0.,1.))
-
-ph <- df %>%
-  group_by(source, distance) %>%
-  pairwise_t_test(answer_timbre ~ movement, paired = TRUE, p.adjust.method = "bonferroni")
-xtable(ph, digits = c(0,0,0,0,0,0,0,0,2,0,3,3,0))
-
-df %>%
-  group_by(source, distance) %>%
-  cohens_d(answer_timbre ~ movement, paired = TRUE)
-
-df %>%
-  ggplot(aes(source, answer_timbre, colour = movement)) +
+  ggplot(aes(angle, answer_plausibility, colour = stimulus)) +
   facet_wrap(. ~ distance) +
   stat_summary(fun = mean, geom = "point", position = position_dodge(width = .1)) +
   stat_summary(fun.data = mean_cl_normal, geom = "errorbar", linewidth = 1, width = .1, position = "dodge") +
   coord_cartesian(ylim=c(-1.,1.))
 
+ph <- df %>%
+  group_by(angle, distance) %>%
+  pairwise_t_test(answer_plausibility ~ stimulus, paired = TRUE)
+ph$p_adj <- ph$p * 24
 
+cohens <- df %>%
+  group_by(angle, distance) %>%
+  cohens_d(answer_plausibility ~ stimulus, paired = TRUE)
+
+ph_tot <- cbind(x = ph[, c('distance', 'angle', 'group1', 'group2', 'p_adj')], y = cohens[, c('effsize')])
+
+xtable(ph_tot, digits = c(0, 0, 0, 0, 0, 3, 3))
+
+# Timbre interaction: Angle x Stimulus x Distance
 df %>%
-  ggplot(aes(movement, answer_timbre, colour = repetition)) +
+  ggplot(aes(room, answer_timbre, colour = angle)) +
   facet_wrap(. ~ source) +
   stat_summary(fun = mean, geom = "point", position = position_dodge(width = .1)) +
   stat_summary(fun.data = mean_cl_normal, geom = "errorbar", linewidth = 1, width = .1, position = "dodge") +
-  coord_cartesian(ylim=c(-1.,1.))
+  coord_cartesian(ylim=c(-1.,1.5))
 
-ph <- df %>%
-  group_by(source, movement) %>%
-  pairwise_t_test(answer_timbre ~ repetition, paired = TRUE, p.adjust.method = "bonferroni")
-xtable(ph, digits = c(0,0,0,0,0,0,0,0,2,0,3,3,0))
-
+# Timbre interaction: Angle x Stimulus x Distance
 df %>%
-  group_by(source, movement) %>%
-  cohens_d(answer_timbre ~ repetition, paired = TRUE)
-
-# DIRECT QUESTIONS
-
-df %>%
-  ggplot(aes(movement, answer_movement, colour = source)) +
-  facet_wrap(. ~ room) +
+  ggplot(aes(angle, answer_timbre, colour = stimulus)) +
+  facet_wrap(. ~ distance) +
   stat_summary(fun = mean, geom = "point", position = position_dodge(width = .1)) +
   stat_summary(fun.data = mean_cl_normal, geom = "errorbar", linewidth = 1, width = .1, position = "dodge") +
-  coord_cartesian(ylim=c(0.5,1.))
+  coord_cartesian(ylim=c(-1.,1.5))
 
-# Exemple interaction: Source x Movement post-hoc (movement moderator)
-sourceXmovement_movementModerator <- df %>%
-  group_by(movement) %>%
-  pairwise_t_test(answer_plausibility ~ source, paired = TRUE, p.adjust.method = "bonferroni")
-
-# Source x Room figure
-fig_sourceXmovement <- df %>%
-  ggplot(aes(movement, answer_timbre, colour = source)) +
+# Direct questions: Angle x Stimulus x Distance
+df %>%
+  ggplot(aes(source, answer_angle, colour = stimulus)) +
+  facet_wrap(. ~ angle) +
   stat_summary(fun = mean, geom = "point", position = position_dodge(width = .1)) +
   stat_summary(fun.data = mean_cl_normal, geom = "errorbar", linewidth = 1, width = .1, position = "dodge") +
-  coord_cartesian(ylim=c(-1.,3.))
-
-# ggsave(fig_source_plausibility, filename = "C:\\Users\\User\\Desktop\\source_plausibility.pdf", device = cairo_pdf)
+  coord_cartesian(ylim=c(0.,1.))
--- src/utils_manip_directivite/analysis/visualization.py
+++ src/utils_manip_directivite/analysis/visualization.py
@@ -3,35 +3,202 @@ import seaborn as sns
 import pandas as pd
 from utils_manip_directivite.analysis.extract_data import get_dataframe
 
-def plot_results(df: pd.DataFrame, question: str):
+Y_LABELS = {
+    'answer_plausibility': 'Plausibility',
+    'answer_timbre': 'Timbre',
+    'answer_angle': 'Angle',
+    'answer_movement': 'Movement'
+}
+
+def plot_global_ratings(df: pd.DataFrame, question: str):
     sns.set_theme()
-    f, axs = plt.subplots(nrows = 2, ncols = 2, sharey = True)
-    sns.boxplot(x = 'movement',
+    f, axs = plt.subplots(nrows = 2, ncols = 4, sharex = True, sharey = True) 
+    f.supylabel('Plausibility score')
+    f.supxlabel('Source')
+    df = df.sort_values(by = ['angle', 'distance', 'stimulus'])
+    angle = ['Front','Front','Side','Side']
+    distance = ['1','4','1','4']
+    room = ['CLOUS','SUAPS']
+    for y in range(2):
+        for x in range(4):
+            sns.violinplot(
                 y = question,
-                hue = 'source',
-                data = df.query('room == "SUAPS" and angle == "Front"'),
-                ax = axs[0,0])
-    sns.boxplot(x = 'movement',
-                y = question,
-                hue = 'source',
-                data = df.query('room == "CLOUS" and angle == "Front"'),
-                ax = axs[0,1])
-    sns.boxplot(x = 'movement',
-                y = question,
-                hue = 'source',
-                data = df.query('room == "SUAPS" and angle == "Side"'),
-                ax = axs[1,0])
-    sns.boxplot(x = 'movement',
-                y = question,
-                hue = 'source',
-                data = df.query('room == "CLOUS" and angle == "Side"'),
-                ax = axs[1,1])
+                x = 'source',
+                hue = 'stimulus',
+                data = df.query(f'room == "{room[y]}" & angle == "{angle[x]}" & distance == {distance[x]}'),
+                ax = axs[y][x],
+                dodge = True   
+            )
+            axs[y][x].legend(title = 'Stimulus')
+            axs[y][x].get_legend().remove()
+            axs[y][x].set_title(f'{angle[x]} - {distance[x]} m')
+            axs[y][x].set_xlabel('')
+            axs[y][x].set_ylabel('')
+
+    for i, ax in enumerate(axs):
+        ax[-1].text(1.5, .5, ['Classroom', 'Sports\nhall'][i])
+    
+    """
+    
+    for ax in axs:
+        ax.set_xlabel('Base facing angle')
+        leg_handles = ax.get_legend_handles_labels()[0]
+        ax.legend(leg_handles, ['Static', 'Dynamic'], title='Movement')
+    plt.subplots_adjust(wspace = .1, right = .95) 
+    """
+    plt.subplots_adjust(left = .05, bottom = .07, right = .95, top = .95, wspace = .05, hspace = .07) 
+    plt.legend(bbox_to_anchor=(.05, 0), loc="lower left",
+                bbox_transform=f.transFigure, ncol=4)
     plt.show()
+    #plt.savefig(r"C:\Users\User\Desktop\plausibility_ThMD.svg")
+
+def plot_ratings_range(df: pd.DataFrame):
+
+    df = df.sort_values(by = ['source','stimulus'])
+    dependant_variables = ['answer_plausibility','answer_timbre']
+    ylabels = ['Plausibility score', 'Timbre score']
+    f, axs = plt.subplots(nrows = 1, ncols = 2, sharey = False)
+    sns.set_theme()
+    for i, ax in enumerate(axs):
+        sns.boxplot(
+            y = dependant_variables[i], 
+            x = 'source',
+            hue = 'stimulus',
+            data = df,
+            dodge = True,
+            ax = ax,
+        )
+        ax.set_ylabel(ylabels[i])
+        ax.get_legend().remove()
+           
+    plt.legend(bbox_to_anchor=(.5, .9), loc="lower center",
+                bbox_transform=f.transFigure, ncol=4)
+    plt.subplots_adjust(wspace = .27, right = .95)
+    #plt.show()
+    plt.savefig(r"C:\Users\User\Desktop\quartiles.svg")
+
+def plot_source_stimulus(df: pd.DataFrame, question: str):
+    sns.set_theme()
+    df = df.sort_values(['source','stimulus'])
+    sns.pointplot(
+        data = df,
+        y = question,
+        x = 'source',
+        hue = 'stimulus',
+        dodge = True,
+        linestyles = ['','','',''],
+        markers = [(4,1,0),(4,1,45),(3,1,0),(3,1,180)],
+        capsize = .05,
+        err_kws = {'linewidth': 2},
+    )
+    plt.ylabel(f'{Y_LABELS[question]} z-score')
+    plt.legend().set_title('Stimulus')
+    plt.show()    
+    # plt.savefig(rf"C:\Users\User\Desktop\{Y_LABELS[question]}_SxSt.svg")
+
+def plot_angle_stimulus_distance(df: pd.DataFrame, question: str):
+    sns.set_theme()
+    df = df.sort_values(['source','stimulus'])
+    distances = ['1', '4']
+    f, axs = plt.subplots(ncols = 2, nrows = 1, sharey = True, sharex = True)
+    for i, ax in enumerate(axs):
+        sns.pointplot(
+            data = df.query(f'distance == {distances[i]}'),
+            y = question,
+            x = 'angle',
+            hue = 'stimulus',
+            dodge = True,
+            linestyles = ['','','',''],
+            markers = [(4,1,0),(4,1,45),(3,1,0),(3,1,180)],
+            capsize = .05,
+            err_kws = {'linewidth': 2},
+            ax = ax
+        )
+        ax.get_legend().set_title('Stimulus')
+        ax.get_legend().remove()
+        ax.set_xlabel('Angle')
+        ax.set_ylabel(f'{Y_LABELS[question]} z-score')
+        ax.set_title(f'{distances[i]} m')
+    
+    plt.legend(bbox_to_anchor=(.5, 1.), loc="upper center",
+                bbox_transform=f.transFigure, ncol=4)
+    plt.subplots_adjust(wspace = .1, right = .95, top = .85)
+    # plt.show()    
+    plt.savefig(rf"C:\Users\User\Desktop\{Y_LABELS[question]}_AxStxD.svg")
+
+def plot_source_angle_stimulus(df: pd.DataFrame, question: str):
+    sns.set_theme()
+    df = df.sort_values(['source','stimulus'])
+    angles = ['Front', 'Side']
+    f, axs = plt.subplots(ncols = 2, nrows = 1, sharey = True, sharex = True)
+    for i, ax in enumerate(axs):
+        sns.pointplot(
+            data = df.query(f'angle == "{angles[i]}"'),
+            y = question,
+            x = 'source',
+            hue = 'stimulus',
+            dodge = True,
+            linestyles = ['','','',''],
+            markers = [(4,1,0),(4,1,45),(3,1,0),(3,1,180)],
+            capsize = .05,
+            err_kws = {'linewidth': 2},
+            ax = ax
+        )
+        ax.get_legend().set_title('Stimulus')
+        ax.get_legend().remove()
+        ax.set_xlabel('Source')
+        ax.set_ylabel(f'{Y_LABELS[question]} good answer ratio')
+        ax.set_title(f'{angles[i]}')
+    
+    plt.legend(bbox_to_anchor=(.5, 1.), loc="upper center",
+                bbox_transform=f.transFigure, ncol=4)
+    plt.subplots_adjust(wspace = .1, right = .95, top = .85)
+    plt.ylim(0., 1.)
+    # plt.show()    
+    plt.savefig(rf"C:\Users\User\Desktop\{Y_LABELS[question]}_AxStxD.svg")
+
+def plot_room_source_angle(df: pd.DataFrame, question: str):
+    sns.set_theme()
+    df = df.sort_values(['source','stimulus'])
+    sources = ['Human', 'Loudspeaker']
+    rooms = ['Classroom', 'Sports hall']
+    angles = ['Front', 'Side']
+    f, axs = plt.subplots(ncols = 2, nrows = 1, sharey = True, sharex = True)
+    for i, ax in enumerate(axs):
+        sns.pointplot(
+            data = df.query(f'source == "{sources[i]}"'),
+            y = question,
+            x = 'room',
+            hue = 'angle',
+            dodge = True,
+            linestyles = ['',''],
+            markers = [(4,1,0),(4,1,45)],
+            capsize = .05,
+            err_kws = {'linewidth': 2},
+            ax = ax
+        )
+        ax.get_legend().set_title('Angle')
+        ax.get_legend().remove()
+        ax.set_xlabel('Room')
+        ax.set_xticks([0,1])
+        ax.set_xticklabels(rooms)
+        ax.set_ylabel(f'{Y_LABELS[question]} z-score')
+        ax.set_title(f'{sources[i]}')
+    
+    plt.legend(bbox_to_anchor=(.5, 1.), loc="upper center",
+                bbox_transform=f.transFigure, ncol=4)
+    plt.subplots_adjust(wspace = .1, right = .95, top = .85)
+    # plt.show()    
+    plt.savefig(rf"C:\Users\User\Desktop\{Y_LABELS[question]}_RxSxA.svg")
 
 def main() -> None:
-    df = get_dataframe(z_score = False)
+    # Export text as text (not path) in svg exports.
+    plt.rcParams['svg.fonttype'] = 'none'
+
+    df = get_dataframe(z_score = True)
     df = df.query('user > 1').sort_values(by=['source'])
-    plot_results(df = df, question = 'answer_timbre')
+
+    plot_source_stimulus(df = df, question = 'answer_timbre')
 
 if __name__ == '__main__':
     main()
