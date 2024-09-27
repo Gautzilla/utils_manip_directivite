2d13f086905d4046ebf0b7c2cc7f3b5a14dcbeb1
Gautzilla <gauthier.berthomieu@gmail.com>
2024-09-27 14:33:55 +0200

    add second func_d

--- src/git_functions.py
+++ src/git_functions.py
@@ -1,5 +1,8 @@
 def func_d():
     print("This was added in branch d")
 
+def second_d_func():
+    print("This was latter added in branch d")
+
 def func_a():
     print('This functions was added to the main branch.')
