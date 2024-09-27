2c24cd082594b4301da56f44109695e3f7461770
Gautzilla <72027971+Gautzilla@users.noreply.github.com>
2024-09-27 14:37:50 +0200

    Merge pull request #3
    * add branch_b commit 1
    
    * add branch_b commit 2
    
    * add branch_c commit 1

--- src/git_functions.py
+++ src/git_functions.py
@@ -5,4 +5,18 @@ def second_d_func():
     print("This was latter added in branch d")
 
 def func_a():
+
+    #functionality added in branch B commit 1:
+    b = 'Salut mec'
+    print(b)
+
+    #functionality added in branch B commit 2:
+    c = "Ca gaze ?"
+    print(c)
+
+
+    # add functionality in branch C:
+    p = [1, 2, 3]
+    print(p)
+
     print('This functions was added to the main branch.')
